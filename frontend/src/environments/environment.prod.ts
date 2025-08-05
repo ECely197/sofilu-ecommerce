f4dc55a5705ff7979af7700c00cca7ecd2cdf1aa
Edwin Cely <edwincely6@gmail.com>
2025-08-05 12:40:57 -0500

    vercel

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'LA_URL_DE_NUESTRO_BACKEND_EN_RENDER', // La llenaremos después
+  apiUrl: 'https://sofilu-ecommerce-api.onrender.com', // La llenaremos después
   firebase: {
     // ... Tus credenciales de Firebase van aquí ...
   }
