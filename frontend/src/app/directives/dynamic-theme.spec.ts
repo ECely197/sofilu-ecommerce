99478fbeaf557fc18403029a10f00b18fbde94e7
Edwin Cely <edwincely6@gmail.com>
2025-09-16 12:10:21 -0500

    components responsive

---
+++ frontend/src/app/directives/dynamic-theme.spec.ts
@@ -0,0 +1,8 @@
+import { DynamicTheme } from './dynamic-theme';
+
+describe('DynamicTheme', () => {
+  it('should create an instance', () => {
+    const directive = new DynamicTheme();
+    expect(directive).toBeTruthy();
+  });
+});
